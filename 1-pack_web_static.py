14b15093a73e0a7f50965ca3053770fdacd6f137
Salma2486 <saloom_001@hotmail.com>
2024-03-06 14:25:51 -0800

    task1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" srgt sryt hrsdy thrdsyth6d"""
+from fabric.api import local
+import os.path
+from datetime import datetime
+
+
+def do_pack():
+    """rftxh srt hsrdt hf"""
+    local("mkdir -p versions")
+    date = datetime.now()
+    date_str = date.strftime("%Y%m%d%H%M%S")
+    local("tar -cvzf versions/web_static_{}.tgz web_static".format(date_str))
+    path = "versions/web_static_{}.tgz".format(date_str)
+    if result.failed:
+        return None
+    else:
+        return (path)
