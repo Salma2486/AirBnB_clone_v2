3be2854657daef3f165b8e4e5cd6190e6b6367ca
Salma2486 <saloom_001@hotmail.com>
2024-03-06 15:15:56 -0800

    qwqwqw

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,10 +9,11 @@ def do_pack():
     local("mkdir -p versions")
     date = datetime.now()
     date_str = date.strftime("%Y%m%d%H%M%S")
-    r = local("tar -cvzf versions/web_static_{}.tgz web_static"
-              .format(date_str))
+    result = local("tar -cvzf versions/web_static_{}.tgz web_static".format(date_str))
     path = "versions/web_static_{}.tgz".format(date_str)
-    if r.failed:
+
+    if result.failed:
         return None
     else:
-        return (path)
+        print("Packing web_static to {}".format(path))
+        return path
