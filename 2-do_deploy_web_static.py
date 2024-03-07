02a15aa75ad902d61c5639a702cafb29fde434cb
Salma2486 <saloom_001@hotmail.com>
2024-03-07 05:51:58 -0800

    task2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,9 @@
 from fabric.api import env, put, run
 from os.path import exists
 
+
 env.hosts = ['54.174.5.167', '18.210.10.255']
+
 
 def do_deploy(archive_path):
     if not exists(archive_path):
@@ -13,13 +15,16 @@ def do_deploy(archive_path):
         filename = archive_path.split("/")[-1]
         name = filename.split(".")[0]
         run("mkdir -p /data/web_static/releases/{}/".format(name))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(filename, name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(filename, name))
         run("rm /tmp/{}".format(filename))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name))
+        run("mv /data/web_static/releases/{}/web_static/* /data/\
+                web_static/releases/{}/".format(name, name))
         run("rm -rf /data/web_static/releases/{}/web_static".format(name))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name))
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(name))
         print("New version deployed!")
         return True
-    except:
+    except Exception as e:
         return False
