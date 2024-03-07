43cdd3b54f9eee74b0bea410cf746e4723a4e336
Salma2486 <saloom_001@hotmail.com>
2024-03-07 07:00:24 -0800

    taskkkkk

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,63 +1,60 @@
 #!/usr/bin/python3
-""" Function that deploys """
-from datetime import datetime
-from fabric.api import *
-import os
-import shlex
+from fabric.api import local, env, put, run
+from time import strftime
+from datetime import date
+from os.path import exists
 
 
-env.hosts = ['18.210.10.255', '54.174.5.167']
-env.user = "ubuntu"
-
-
-def deploy():
-    """ DEPLOYS """
-    try:
-        archive_path = do_pack()
-    except:
-        return False
-
-    return do_deploy(archive_path)
+env.hosts = ['54.174.5.167', '18.210.10.255']
 
 
 def do_pack():
+    """ A script that generates archive the contents of web_static folder"""
+
+    filename = strftime("%Y%m%d%H%M%S")
     try:
-        if not os.path.exists("versions"):
-            local('mkdir versions')
-        t = datetime.now()
-        f = "%Y%m%d%H%M%S"
-        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
-        local('tar -cvzf {} web_static'.format(archive_path))
-        return archive_path
-    except:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
+
+        return "versions/web_static_{}.tgz".format(filename)
+
+    except Exception as e:
         return None
 
 
 def do_deploy(archive_path):
-    """ Deploys """
-    if not os.path.exists(archive_path):
+    """Fabric script that distributes
+    an archive to your web server"""
+    if not exists(archive_path):
         return False
+
     try:
-        name = archive_path.replace('/', ' ')
-        name = shlex.split(name)
-        name = name[-1]
-
-        wname = name.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
-
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
-
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
+        filename = archive_path.split("/")[-1]
+        name = filename.split(".")[0]
+        run("mkdir -p /data/web_static/releases/{}/".format(name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(filename, name))
+        run("rm /tmp/{}".format(filename))
+        run("mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".format(name, name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(name))
         print("New version deployed!")
         return True
-    except:
+    except Exception as e:
+        print("An error occurred:", e)
         return False
+
+
+def deploy():
+    """run the 2 functions"""
+
+    path = do_pack()
+    if not path:
+        return False
+
+    return do_deploy(path)
